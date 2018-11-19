9fe0623bd50dde01ee820881329d25ffb16ccfbe
MPuhakka <mika.puhakka@geniem.com>
2018-11-19 09:54:40 +0200

    local build script, changed image name

--- build_local.sh
+++ build_local.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 meteor build --allow-superuser --server-only --architecture os.linux.x86_64 ..
 mv ../rocket.tar.gz build.tar.gz
-docker build . -f .docker/Dockerfile -t devgeniem/rocket.chat:testi
-docker push devgeniem/rocket.chat:testi
+docker build . -f .docker/Dockerfile -t devgeniem/rocket.chat:latest
+docker push devgeniem/rocket.chat:latest
