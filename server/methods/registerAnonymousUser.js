18fc2d330c0d106286e05edf4d03ef45c1ebe90d
Topi Latva-Salo <topi.latva-salo@geniem.com>
2019-01-29 09:48:06 +0200

    Clean up the code

--- server/methods/registerAnonymousUser.js
+++ server/methods/registerAnonymousUser.js
@@ -24,39 +24,5 @@ Meteor.methods({
 		const { id, token } = Accounts._loginUser(this, userId);
 
 		return { id, token };
-
-
-		// const user = RocketChat.models.Users.findOneById(userId);
-
-		// if (user) {
-		// 	RocketChat.models.Users.setUserActive(userId, active);
-
-		// 	if (user.username) {
-		// 		RocketChat.models.Subscriptions.setArchivedByUsername(user.username, !active);
-		// 	}
-
-		// 	if (active === false) {
-		// 		RocketChat.models.Users.unsetLoginTokens(userId);
-		// 	} else {
-		// 		RocketChat.models.Users.unsetReason(userId);
-		// 	}
-
-		// 	const destinations = Array.isArray(user.emails) && user.emails.map(email => `${ user.name || user.username }<${ email.address }>`);
-
-		// 	if (destinations) {
-		// 		const email = {
-		// 			to: destinations,
-		// 			from: RocketChat.settings.get('From_Email'),
-		// 			subject: Accounts.emailTemplates.userActivated.subject({active}),
-		// 			html: Accounts.emailTemplates.userActivated.html({active, name: user.name, username: user.username})
-		// 		};
-
-		// 		Meteor.defer(() => Email.send(email));
-		// 	}
-
-		// 	return true;
-		// }
-
-		// return false;
 	}
 });
